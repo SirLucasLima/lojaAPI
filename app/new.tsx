25537540abb09c0c7677aa958a71caebfbba4895
Developer Lucas Lima <developerlucaslima@gmail.com>
2023-05-24 12:29:47 -0300

    somes improves at newMemory

--- app/new.tsx
+++ app/new.tsx
@@ -88,30 +88,30 @@ export default function NewMemory() {
     >
       <View className="mt-4 flex-row items-center justify-between">
         <NLWLogo />
+
         <Link href="/memories" asChild>
           <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-purple-500">
             <Icon name="arrow-left" size={16} color="#fff" />
           </TouchableOpacity>
         </Link>
       </View>
+
       <View className="mt-6 space-y-6">
         <View className="flex-row items-center gap-2">
           <Switch
             value={isPublic}
             onValueChange={setIsPublic}
-            trackColor={{
-              false: '#767577',
-              true: '#372568',
-            }}
+            trackColor={{ false: '#767577', true: '#372560' }}
             thumbColor={isPublic ? '#9b79ea' : '#9e9ea0'}
           />
           <Text className="font-body text-base text-gray-200">
-            Make the memory public
+            Make this public
           </Text>
         </View>
+
         <TouchableOpacity
-          onPress={openImagePicker}
           activeOpacity={0.7}
+          onPress={openImagePicker}
           className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
         >
           {preview ? (
@@ -122,8 +122,8 @@ export default function NewMemory() {
             />
           ) : (
             <View className="flex-row items-center gap-2">
-              <Icon name="image" color="#fff"></Icon>
-              <Text className="font-body text-sm text-gray-200">
+              <Icon name="image" color="#fff" />
+              <Text className="text-sm font-bold text-gray-200">
                 Add cover photo or video
               </Text>
             </View>
@@ -136,14 +136,14 @@ export default function NewMemory() {
           onChangeText={setContent}
           textAlignVertical="top"
           className="p-0 font-body text-lg text-gray-50"
-          placeholder="Feel free to add photos, videos, and narratives about this experience that you want to remember forever."
           placeholderTextColor="#56565a"
+          placeholder="Feel free to add photos, videos, and narratives about this experience that you want to remember forever."
         />
 
         <TouchableOpacity
           activeOpacity={0.7}
           onPress={handleCreateMemory}
-          className="items-center rounded-full bg-green-500 px-5 py-2"
+          className="items-center self-end rounded-full bg-green-500 px-5 py-2"
         >
           <Text className="font-alt text-sm uppercase text-black">Save</Text>
         </TouchableOpacity>
